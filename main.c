3774848cbf520dad956e7d879a74d6ba23c32227
makar <makar.ugolnikov@stud.tsu.ru>
2026-02-16 13:42:59 +0700

    fix \302 spaces and import

--- main.c
+++ main.c
@@ -8,21 +8,22 @@
 
 
 #include <stdio.h>
+#include <stdbool.h>
+#define szz 10
 
 
 int main()
 {
-    const int szz = 10;
-    int array[szz] = {5,5,7,3,3,2,2,1,1,10};
-    int max = array[0];
-    for(int i=1;i < szz;i++)
-        if(array[i] > max) max = array[i];
-    bool unique = true;
-    int notExist = max+1;
-    int countEl=0;
-    for(int i=0;i < szz;i++)
-    {
-        int jx = i+1;
+    int array[szz] = {-1,3,1,1,1,1,1,1,1,2};
+    int max = array[0];
+    for(int i=1;i < szz;i++)
+      if(array[i] > max) max = array[i];
+    bool unique = true;
+    int notExist = max+1;
+    int countEl=0;
+    for(int i=0;i < szz;i++)
+    {
+        int jx = i+1;
         if(array[i]!= notExist)
         {
             unique = true;
@@ -38,14 +39,15 @@ int main()
                 }
                 jx++;
             }
-        }
-        else
-            unique = false;
-        if(unique)
-            countEl++;
-        else
+        }
+        else
+            unique = false;
+        if(unique)
+            countEl++;
+        else
             array[i] = notExist;
-    }
+    }
 
-    printf("countEl = %d", countEl);
+    printf("countEl = %d", countEl);
+return 0;
 }
